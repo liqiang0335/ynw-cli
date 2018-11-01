9d821f9cc30ea0b4db0e0f72364d3e9692cb59d4
liqiang <liqiang0335@qq.com>
2018-11-01 11:05:49 +0800

    add gulp commond

---
+++ command/sprite.js
@@ -0,0 +1,51 @@
+const path = require("path");
+let gulp, spritesmith;
+
+module.exports = context => {
+  const { fns } = context;
+  fns.load("colors");
+  try {
+    gulp = fns.load("gulp");
+    spritesmith = fns.load("gulp.spritesmith");
+    main();
+  } catch (e) {
+    console.log(`cnpm i --save-dev gulp gulp.spritesmith`.red);
+    console.log("请先安装依赖模块后重新运行".red);
+  }
+};
+
+function main() {
+  const cwd = process.cwd();
+  const config = require(path.join(cwd, "./ynw.config.js"));
+  let gulpConfig = config.gulp;
+  if (!gulpConfig) {
+    console.log(`ynw.config未配置gulp:{src,dist}参数`.red);
+    return;
+  }
+
+  const cssTemplate = images => images.sprites.map(transfer).join("");
+  const spriteData = gulp.src(gulpConfig.src + "/*.*").pipe(
+    spritesmith({
+      imgName: "sprite.png",
+      cssName: "sprite.css",
+      padding: 10,
+      cssTemplate
+    })
+  );
+  spriteData.pipe(gulp.dest(gulpConfig.dist));
+
+  function transfer(item) {
+    const name = item.name;
+    const bg = item.escaped_image;
+    const x = item.px.offset_x;
+    const y = item.px.offset_y;
+    const w = item.px.width;
+    const h = item.px.height;
+    return `
+    .sp-${name}
+  { background-image: url(./${bg});
+     background-position: ${x} ${y};
+     width: ${w};height: ${h};}
+  `;
+  }
+}
