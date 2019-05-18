b1c18a1f6313a6004a56c0541c21e3ffa7fe93d5
liqiang0335@qq.com <liqiang0335@gmail.com>
2019-05-19 01:22:29 +0800

    remove copy console

--- commands/build/options/plugins/copy.js
+++ commands/build/options/plugins/copy.js
@@ -2,7 +2,6 @@ module.exports = ctx => {
   const { load, copy } = ctx;
   if (Array.isArray(copy) && copy.length > 0) {
     const CopyWebpackPlugin = load("copy-webpack-plugin");
-    console.log(">>>".red, copy);
     return new CopyWebpackPlugin(copy);
   }
 };
