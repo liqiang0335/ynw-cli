c289e53e042e806eb08b5b4861e8e665334ca77a
liqiang0335@qq.com <liqiang0335@gmail.com>
2019-05-26 01:26:13 +0800

    add console

--- commands/build/options/optionDecorator.js
+++ commands/build/options/optionDecorator.js
@@ -1,5 +1,6 @@
 module.exports = options => {
   if (options.target !== "web") {
+    console.log("alias of 'require': '__non_webpack_require__'".green);
     options.node = {
       __dirname: false,
       __filename: false
--- commands/build/options/rules/lessLoader.js
+++ commands/build/options/rules/lessLoader.js
@@ -9,6 +9,10 @@ module.exports = ({ projectPath, themePath }) => {
   let modifyVars = local || common;
   const lessOptions = modifyVars ? { modifyVars } : null;
 
+  if (lessOptions) {
+    console.log("use antd theme".green);
+  }
+
   return {
     loader: "less-loader",
     options: {
