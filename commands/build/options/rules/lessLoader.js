cc7aed4b7cda2c6f9344fff77ac7f7f2acf46d99
liqiang <liqiang0335@qq.com>
2019-05-28 17:16:48 +0800

    update

--- commands/build/options/rules/lessLoader.js
+++ commands/build/options/rules/lessLoader.js
@@ -9,10 +9,6 @@ module.exports = ({ projectPath, themePath }) => {
   let modifyVars = local || common;
   const lessOptions = modifyVars ? { modifyVars } : null;
 
-  if (lessOptions) {
-    console.log("> use antd theme".green);
-  }
-
   return {
     loader: "less-loader",
     options: {
--- commands/build/options/rules/sass-resources.js
+++ commands/build/options/rules/sass-resources.js
@@ -4,7 +4,6 @@ const path = require("path");
 module.exports = ({ projectPath }) => {
   const localPath = path.join(projectPath, "/style/var.scss");
   if (fs.existsSync(localPath)) {
-    console.log("> use sass variable".green);
     return {
       loader: "sass-resources-loader",
       options: { resources: localPath }
