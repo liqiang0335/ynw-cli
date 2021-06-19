fe24ac154b1219988ec3064407ac8a5895015253
liqiang0335@qq.com <liqiang0335@gmail.com>
2021-06-19 11:27:59 +0800

    add plugin

--- commands/build/options/plugins/index.js
+++ commands/build/options/plugins/index.js
@@ -5,11 +5,11 @@ const copy = require("./copy");
 const dllPlugin = require("./dll");
 
 module.exports = ctx => {
-  const { isHot, fns, load } = ctx;
+  const { isHot, fns, load, plugins } = ctx;
   const toArray = fns.toArray;
   const webpack = load("webpack");
   const VueLoaderPlugin = load("vue-loader/lib/plugin");
-  let result = [new VueLoaderPlugin()];
+  let result = [new VueLoaderPlugin(), ...plugins];
   if (isHot) {
     result.push(new webpack.HotModuleReplacementPlugin());
     result.push(new webpack.NamedModulesPlugin());
