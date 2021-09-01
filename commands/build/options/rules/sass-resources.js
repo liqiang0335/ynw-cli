c2ffd26670239adeb9142e41c42189357b0960bb
liqiang0335 <liqiang0335@qq.com>
2021-09-01 16:57:58 +0800

    add

--- commands/build/options/rules/sass-resources.js
+++ commands/build/options/rules/sass-resources.js
@@ -1,12 +1,12 @@
 const fs = require("fs");
 const path = require("path");
 
-module.exports = ({ projectPath }) => {
-  const localPath = path.join(projectPath, "/style/var.scss");
+module.exports = ({ projectPath, sassVarPath = "/style/var.scss" }) => {
+  const localPath = path.join(projectPath, sassVarPath);
   if (fs.existsSync(localPath)) {
     return {
       loader: "sass-resources-loader",
-      options: { resources: localPath }
+      options: { resources: localPath },
     };
   }
 };
