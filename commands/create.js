c0a93b6c6a45493c2604e0eadc3b2950e60eff6c
liqiang0335 <liqiang0335@qq.com>
2021-12-09 15:20:15 +0800

    add

--- commands/create.js
+++ commands/create.js
@@ -1,3 +1,33 @@
-module.exports = context => {
-  console.log("hello create !!!!!!!!!!!");
+const inquirer = require("inquirer");
+const path = require("path");
+const questions = [
+  {
+    type: "list",
+    name: "template",
+    message: "请选择模板",
+    choices: ["tablelist"],
+  },
+];
+
+module.exports = function (ctx) {
+  for (let key in handlers) {
+    if (ctx[key]) {
+      handlers[key](ctx);
+      return;
+    }
+  }
+  inquirer.prompt(questions).then(answers => {
+    handlers[answers.template](ctx);
+  });
+};
+
+const handlers = {
+  tablelist(ctx) {
+    if (!ctx.path) {
+      return console.error("请输入path路径参数".red);
+    }
+    if (!/[\\/]/.test(ctx.path)) {
+      return console.error("path路径使用双引号括起来".red);
+    }
+  },
 };
